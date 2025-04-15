8a4681f36bc1a925664e1bb52c34cd8e4d44824d
Tatiana Chaikina <introbox87@gmail.com>
2025-04-15 18:35:45 +0300

    Campgrounds

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
