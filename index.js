719ef2788dcf3e559c6a38d53da6995b50f684d7
Tatiana Chaikina <introbox87@gmail.com>
2025-04-16 17:36:58 +0300

    Campground index

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.get('/', async (req, res) => {
     res.render('home');
 });
 
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+});
+
 app.listen(3000, () => {
     console.log("App is listening on port 3000");
 })
