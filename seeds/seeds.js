980da63ede4c3eb44d09406144caa4a07e87b7d8
Tatiana Chaikina <introbox87@gmail.com>
2025-04-20 23:33:37 +0300

    Styling views

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const axios = require('axios');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
@@ -13,18 +14,38 @@ db.once('open', () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async () => {
+async function seedImg() {
+    try {
+      const resp = await axios.get('https://api.unsplash.com/photos/random', {
+        params: {
+          client_id: '7Q4BH28pVNlD8x5wE2JDRUIh6WX9_YNFeyizPXD_W8M',
+          collections: 1114848,
+        },
+      })
+      return resp.data.urls.small;
+    } catch (err) {
+      console.error(err);
+      // Возвращаем fallback изображение в случае ошибки
+      return 'https://source.unsplash.com/collection/483251';
+    }
+  }
+  
+  const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: await seedImg(), // Используем функцию seedImg()
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        });
         await camp.save();
     }
-};
-
-seedDB().then(() => {
-    mongoose.connection.close()
-});
+  }
+  
+  seedDB().then(() => {
+    mongoose.connection.close();
+  });
