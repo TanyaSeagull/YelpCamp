6256377045d3eef90f4afe3bd3d7459bc44416e6
Tatiana Chaikina <introbox87@gmail.com>
2025-05-15 19:50:17 +0300

    Flash errors

--- routes/reviews.js
+++ routes/reviews.js
@@ -20,8 +20,6 @@ const validateReview = (req, res, next) => {
     }
 }
 
-
-
 router.post('/', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
@@ -30,7 +28,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     await campground.save();
     req.flash('success', 'Created new review!');
     res.redirect(`/campgrounds/${campground._id}`);
-}))
+}));
 
 router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
@@ -38,6 +36,6 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     await Review.findByIdAndDelete(reviewId);
     req.flash('success', 'Successfully deleted review')
     res.redirect(`/campgrounds/${id}`);
-}))
+}));
 
 module.exports = router;
