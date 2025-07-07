b86fc70c9069832edb55148900472084f9a32e87
Tatiana Chaikina <introbox87@gmail.com>
2025-07-07 14:02:36 +0300

    Added an Author to Campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync.js');
 const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
 
 const ExpressError = require('../utils/expressError.js');
 const Campground = require('../models/campground.js');
@@ -21,20 +22,22 @@ router.get('/', catchAsync(async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
   }));
   
-  router.get('/new', (req, res) => {
+  router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
   });
   
-  router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+  router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
   }));
   
-  router.get('/:id', catchAsync(async (req, res,) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+  router.get('/:id', isLoggedIn, catchAsync(async (req, res,) => {
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    console.log(campground);
     if (!campground) {
       req.flash('error', 'Cannot find that campground!');
       return res.redirect('/campgrounds');
