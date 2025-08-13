7e9187b6e353b39e78fef3fa082234bb11066393
Tatiana Chaikina <introbox87@gmail.com>
2025-08-13 11:57:39 +0300

    Image Upload

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -45,10 +45,10 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    if (req.files && req.files.length > 0) {
-        campground.images.push(...req.files.map(f => ({ url: f.path, filename: f.filename })));
-        await campground.save();
-    }
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
+    
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }
