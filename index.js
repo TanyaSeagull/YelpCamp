a7a1d0a6963844222950dcefd6a14ffea466a234
Tatiana Chaikina <introbox87@gmail.com>
2026-03-18 11:20:22 +0300

    Error re-name

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const express = require('express');
 const app = express();
 app.set('query parser', 'extended');
 const engine = require('ejs-mate');
-const ExpressError = require('./utils/expressError');
+const ExpressError = require('./utils/ExpressError.js');
 const methodOverride = require('method-override');
 const path = require('path');
 const mongoose = require('mongoose');
