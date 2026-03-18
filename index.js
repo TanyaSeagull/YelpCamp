0d56a81c37c0342bf8d8e40adcfbbfc7ae7f230e
Tatiana Chaikina <introbox87@gmail.com>
2026-03-18 12:07:47 +0300

    Mongoose.connect

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
