bbc641de161cf42fef3db6079259377d7e1dbbc0
Tatiana Chaikina <introbox87@gmail.com>
2026-07-15 19:34:41 +0300

    geocoding updated

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -38,14 +38,25 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
-    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
-    const campground = new Campground(req.body.campground);
-    campground.geometry = geoData.features[0].geometry;
-    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 }); //
+    const campground = new Campground(req.body.campground); //
+    
+    // Безопасная запись геоданных
+    if (geoData.features && geoData.features.length > 0) {
+        campground.geometry = geoData.features[0].geometry;
+    } else {
+        // Дефолтные координаты (0, 0), если адрес не найден
+        campground.geometry = {
+            type: "Point",
+            coordinates: [0, 0]
+        };
+    }
+    
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename })); //
+    campground.author = req.user._id; //
+    await campground.save(); //
+    req.flash('success', 'Successfully made a new campground!'); //
+    res.redirect(`/campgrounds/${campground._id}`); //
 }
 
 module.exports.showCampground = async (req, res) => {
@@ -73,22 +84,39 @@ module.exports.renderEditForm = async (req, res) => {
 }
 
 module.exports.updateCampground = async (req, res) => {
-    const { id } = req.params;
-    console.log(req.body);
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
-    campground.geometry = geoData.features[0].geometry;
-    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
-    campground.images.push(...imgs);
-    await campground.save();
+    const { id } = req.params; 
+    const campground = await Campground.findById(id); 
+    
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+
+    // Обновляем текстовые поля вручную
+    Object.assign(campground, req.body.campground);
+
+    // Геокодинг с проверкой безопасности
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 }); //[cite: 2]
+    if (geoData.features && geoData.features.length > 0) {
+        campground.geometry = geoData.features[0].geometry;
+    }
+
+    // Добавляем новые картинки
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename })); 
+    campground.images.push(...imgs); 
+
+    // Удаляем картинки, если отмечены
     if (req.body.deleteImages) {
         for (let filename of req.body.deleteImages) {
-            await cloudinary.uploader.destroy(filename);
+            await cloudinary.uploader.destroy(filename); 
         }
-        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+        // Вместо лишнего запроса к БД через updateOne, удаляем их прямо из локального массива:
+        campground.images = campground.images.filter(img => !req.body.deleteImages.includes(img.filename));
     }
-    req.flash('success', 'Successfully updated campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
+
+    await campground.save(); 
+    req.flash('success', 'Successfully updated campground!'); 
+    res.redirect(`/campgrounds/${campground._id}`); 
 };
 
 module.exports.deleteCampground = async (req, res) => {
