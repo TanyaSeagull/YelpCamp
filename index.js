57cb6cdaef73bb46c130d870aa90b38723174d92
Tatiana Chaikina <introbox87@gmail.com>
2026-07-21 18:40:14 +0300

    secure: true

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
       httpOnly: true,
-      // secure: true, // Можно раскомментировать, так как мы добавили trust proxy выше
+      secure: true, 
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7
   }
